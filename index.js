72a9ef3428080d1acb5c7836dd27760e7bb7df81
johnfruscella <johnfruscella@gmail.com>
2020-02-02 16:43:44 -0500

    Connected to database and created schema for data. Then created model

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const mongoose = require('mongoose');
 
 const object = {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
 };
 mongoose.connect('mongodb://localhost/playground', object)
-.then(()=> console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to that MongoDB...', err));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to that MongoDB...', err));
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+//Making Model. Create schema, compile into model which gives a class. Create object based on Class
+//Classes, objects: objects are an instance of a Class. A class is a blueprint
+//Course, nodeCourse
+async function createCourse() {
+    const Course = mongoose.model('Course', courseSchema); //Course is class, ergo Pascal naming convention
+    const course = new Course({
+        name: 'Chemistry Course',
+        Author: 'Dawkins',
+        tags: ['atoms', 'inorganic'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+createCourse();
