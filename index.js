23ed2f384ffc12c42589234eec6e8c2ffe93a831
johnfruscella <johnfruscella@gmail.com>
2020-02-04 18:10:25 -0500

    Added comparison operators to getCourses function

--- index.js
+++ index.js
@@ -20,23 +20,33 @@ const courseSchema = new mongoose.Schema({
 //Course, nodeCourse
 const Course = mongoose.model('Course', courseSchema); //Course is class, ergo Pascal naming convention
 async function createCourse() {
-
     const course = new Course({
-        name: 'Physics Course',
-        author: 'Dawkins',
-        tags: ['velocity', 'matter'],
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
         isPublished: true
     });
 
     const result = await course.save();
     console.log(result);
 }
+//createCourse();
 
 async function getCourses() {
+    //Comparison Operators: use $ in front to indicate use of operator (e.g. $gt)
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in)
     const courses = await Course
-        .find({ isPublished: true })
+        //.find({ author: 'Mosh', isPublished: true })
+        //.find({ price: { $gte: 10, $lte: 20 } }) find course between 10 and 20
+        .find({ price: { $in: [10, 15,20] } }) // find course at value 10, 15, and 20
         .limit(10)
-        .sort({ name: 1 })  //sorts name is ascending order. -1 for descending
+        .sort({ name: 1 })  //1 sorts name is ascending order. -1 for descending
         .select({ name: 1, tags: 1 });
     console.log(courses);
 };
