1e8e673a38fb04d8cf0e5724cf034dde83ed5e90
johnfruscella <johnfruscella@gmail.com>
2020-02-07 23:52:04 -0500

    added the ability to delete course by id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost/playground', object)
     .catch(err => console.error('Could not connect to that MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     author: String,
     tags: [String],
     date: { type: Date, default: Date.now },
@@ -19,16 +19,22 @@ const courseSchema = new mongoose.Schema({
 //Classes, objects: objects are an instance of a Class. A class is a blueprint
 //Course, nodeCourse
 const Course = mongoose.model('Course', courseSchema); //Course is class, ergo Pascal naming convention
+
 async function createCourse() {
     const course = new Course({
-        name: 'Angular Course',
+        //name: 'Angular Course',
         author: 'Mosh',
         tags: ['angular', 'frontend'],
         isPublished: true
     });
 
+    //try{
     const result = await course.save();
     console.log(result);
+    //} catch (ex) { //ex is for exception
+    //     console.log(ex.message);
+    // };
+
 }
 
 
@@ -42,16 +48,12 @@ async function getCourses() {
         .limit(pageSize)
         .sort({ name: 1 })  //1 sorts name is ascending order. -1 for descending
         .select({ name: 1, tags: 1 });
-        //.countDocuments(); // returns the number of documents in the database. countDocuments() replaces the deprecated count(). 
+    //.countDocuments(); // returns the number of documents in the database. countDocuments() replaces the deprecated count(). 
     console.log(courses);
 };
 
-async function updateCourse(id){
-// Second approach is Update first
-// update directly in database
-// Optionally: get the updated document
-
-    const result = await Course.updateOne({_id: id}, { // NOTE: '.update' is deprecated use 'updateOne', updateMany, or bulkWrite instead
+async function updateCourse(id) {
+    const result = await Course.updateOne({ _id: id }, { // NOTE: '.update' is deprecated use 'updateOne', updateMany, or bulkWrite instead
         $set: {
             author: 'Mosh',
             isPublished: false
@@ -59,8 +61,14 @@ async function updateCourse(id){
     });
 
     console.log(result);
-    
+
 }
-updateCourse('5e39f45001a7d31e0064dcc3');
-//getCourses();
+async function removeCourse(id) {
+    const result = await Course.deleteOne({ _id: id });
+    console.log(result);
+
+}
+//updateCourse('5e39f45001a7d31e0064dcc3');
+removeCourse('5e39f45001a7d31e0064dcc3');
+getCourses();
 //createCourse();
